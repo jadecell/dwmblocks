1d4415da422cd33a8f9e66044ff092f98bfb936e
Jackson McCrory <jackson@mccrory.xyz>
2021-04-04 20:32:06 -0700

    Switched around some modules

--- config.h
+++ config.h
@@ -3,14 +3,13 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 
     {"", "sb-cpuperc dwm",				5,		                9},
-	{"", "sb-pacupdate dwm",			18000,	                8},
 	{"", "sb-memory dwm",				5,		                7},
-	{"", "sb-corona",			        18000,	                6},
-	{"", "sb-forecast",				    18000,	                5},
+	{"", "sb-corona",			        21600, 		            6},
+	{"", "sb-forecast",				    21600,		            5},
 	{"", "sb-cputemp desktop",	        5,		                4},
 	{"", "sb-volume dwm",				0,		                3},
-	{"", "sb-clock dwm",				60,		                2},
-	{"", "sb-networking",				5,		                1},
+	{"", "sb-clock dwm",				1,		                2},
+	{"", "sb-networking",				2,		                1},
 
 };
 
