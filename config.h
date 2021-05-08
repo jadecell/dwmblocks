856db7dc17909856d51ab83956dc116706cafc84
Jackson McCrory <jackson@mccrory.xyz>
2021-05-08 00:21:53 -0700

    added news and removed forecast

--- config.h
+++ config.h
@@ -3,11 +3,11 @@
 static const Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
 
+    {"", "sb-news", 0, 6},
     {"", "sb-torrent", 1, 10},
     {"", "sb-cpuperc dwm", 5, 9},
     {"", "sb-pacupdate dwm", 21600, 8},
     {"", "sb-memory dwm", 5, 7},
-    {"", "sb-forecast", 21600, 5},
     {"", "sb-cputemp desktop", 5, 4},
     {"", "sb-volume dwm", 0, 3},
     {"", "sb-clock dwm", 1, 2},
