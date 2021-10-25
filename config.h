9a110b4fbb0368e7af70038cff9c6fd945653c6b
Jackson McCrory <jackson@mccrory.xyz>
2021-10-24 18:08:23 -0700

    own git to github.

--- config.h
+++ config.h
@@ -3,15 +3,17 @@
 static const Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
 
-    {"", "sb-spotify", 1, 11},
-    {"", "sb-news", 0, 6},
-    {"", "sb-torrent", 1, 10},
-    {"", "sb-cpuperc dwm", 5, 9},
-    {"", "sb-memory dwm", 5, 7},
-    {"", "sb-cputemp desktop", 5, 4},
-    {"", "sb-volume dwm", 0, 3},
-    {"", "sb-clock dwm", 1, 2},
-    {"", "sb-networking dwm", 1, 1},
+    //{"", "sb-mail", 5, 11},
+    //{"", "sb-news", 0, 6},
+    //{"", "sb-torrent", 5, 10},
+    //{"", "sb-cpuperc dwm", 5, 9},
+    //{"", "sb-memory dwm", 5, 7},
+    //{"", "sb-packcount dwm", 1, 8},
+    //{"", "sb-cputemp", 5, 4},
+    //{"", "sb-volume dwm", 0, 3},
+    //{"", "sb-upt dwm", 15, 5},
+    {"", "/home/jackson/.local/bin/statusbar/sb-clock nerd", 1, 2},
+    //{"", "sb-networking dwm", 1, 1},
 
 };
 
